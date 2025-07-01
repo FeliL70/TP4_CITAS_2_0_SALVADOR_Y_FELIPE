3b388459d8f8bc50c91af158fc7e347176a73311
FeliL70 <felipelipretizelaya@gmail.com>
2025-07-01 19:36:35 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-   base: '/TP4_CITAS.2.0_SALVADOR_Y_FELIPE/',
+   base: '/TP4_CITAS_2_0_SALVADOR_Y_FELIPE/',
   plugins: [react()],
 })
